73041a9c55550cb0f5f4c191b74430900b5fb2bb
daimoc <dfetis@gmail.com>
2019-10-03 23:21:47 +0200

    First commit with an old script. Not tested since some time so it may be broken.

---
+++ README.md
@@ -0,0 +1,40 @@
+## Synopsis
+
+This project is made to provide a simple Audio an Video WebRTC to SIP gateway using the WebRTC possibility of new Astersik versions.
+It's based on https://github.com/asterisk/cyber_mega_phone_2k
+
+## Motivation
+
+This gateway was made to easily connect a browser to any classic SIP endpoint like Softphone, PABX or MCU.
+
+## Usefull link to build an Asterisk SIP-webrtc gateway
+https://github.com/asterisk/cyber_mega_phone_2k
+https://wiki.asterisk.org/wiki/display/AST/Installing+Asterisk+From+Source
+https://wiki.asterisk.org/wiki/display/AST/WebRTC+tutorial+using+SIPML5
+https://webrtc.ventures/2018/03/webrtc-sip-the-demo/
+
+
+## Installation on Ubuntu 18.04.03 LTS
+```bash
+sh install.sh
+```
+
+
+
+
+## Contributors
+Damien Fétis
+
+## License
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
---
+++ install.sh
@@ -0,0 +1,69 @@
+#!/bin/sh
+
+# Astersi webrtc installation script with self sign certificat
+
+#Asterisk installation
+
+INST_ROOT=/vagrant/
+apt-get update
+
+cd /usr/local/src
+
+wget http://downloads.asterisk.org/pub/telephony/asterisk/asterisk-16-current.tar.gz
+
+
+tar -zxvf asterisk-16-current.tar.gz
+
+rm asterisk-16-current.tar.gz
+cd /usr/local/src/asterisk*
+
+#cd contrib/scripts
+
+#./install_prereq install
+
+#cd ../..
+
+sudo apt install -y wget gcc g++ ncurses-dev libxml2-dev libsqlite3-dev \
+libsrtp-dev uuid-dev libssl-dev libjansson-dev build-essential
+
+./configure
+make && make install
+
+#sleect res_crypto, res_http_websocket, and res_pjsip_transport_websocket and opus
+
+make samples && make config
+
+#Astersisk webrtc Installation
+
+
+mkdir /etc/asterisk/keys
+
+cd /usr/local/src/asterisk-16/contrib/scripts
+ ./ast_tls_cert -C pbx.example.com -O "My Super Company" -d /etc/asterisk/keys
+
+
+#cat $INST_ROOT/asterisk_config/http.conf >> /etc/asterisk/http.conf
+
+#cat $INST_ROOT/asterisk_config/pjsip.conf >> /etc/asterisk/pjsip.conf
+
+
+#configure turn server
+
+#cat $INST_ROOT/asterisk_config/extentions.conf >> /etc/asterisk/extentions.conf
+
+##asterisk -rx "restart"#
+
+
+#cyber_mega_phone_2k installation
+#cd /usr/local
+apt-get -y install git nginx
+#git clone https://github.com/asterisk/cyber_mega_phone_2k.git
+
+cd /usr/local
+git clone https://github.com/agilityfeat/webrtc-sip-example.git
+
+cd webrtc-sip-example
+cp -f asterisk-conf/* /etc/asterisk
+#push nginx configure
+
+service nginx restart
